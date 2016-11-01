e2887595b971afacc4b62e0c9da8255865308a96
Connie.Yu <connie.yu@target.com>
2016-10-31 22:53:35 -0500

    milestone 3

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,11 +2,18 @@
  * Created by z037900 on 10/29/16.
  */
 'use strict';
+var Server = require('./server.js').Server;
+var server = Server('8080');
 
+server.listen(function(){
+    console.log('Server started and listening on port',
+    server.options.port);
+});
+/**
 var Percolator = require('percolator').Percolator;
 var dbSession = require('../../src/backend/dbSession.js');
 
-var port = 8080;
+var port = 8081;  // test only
 var server = Percolator({'port': port, 'autoLink': false});
 
 server.route('/api/keywords',
@@ -28,3 +35,5 @@ server.route('/api/keywords',
 server.listen(function(){
     console.log('Server started and listening on port', port);
 });
+
+ */
